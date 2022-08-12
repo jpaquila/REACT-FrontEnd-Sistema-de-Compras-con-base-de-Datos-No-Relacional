bf3098ddee7f5f0b580f59163c043f464d683433
Juan Pablo Aquila <jpaquila@gmail.com>
2022-08-11 22:55:10 -0300

    Presentación 1 - Clase 2 - React

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        h1 > JUAN PABLO AQUILA < /h1> <
+        h2 > React - Clase 2 - Presentación 1 < /h2> <
+        p >
+        Edit < code > src / App.js < /code> and save to reload. <
+        /p> <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
